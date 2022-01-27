b12d1aa9326be953357f809546527cbe89e05790
matt wartell <matt.wartell@twosixlabs.com>
2022-01-27 10:15:26 -0500

    commit 0

--- merges/commit.sh
+++ merges/commit.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 set -e
 
-git checkout posion
-count=$(wc -l poison.txt)
+git checkout $branch
+count=$(wc -l < poison.txt)
 echo $count $(date) >> posion.txt
 git add -A
 
-echo "commit: creating poison commit $count"
+echo "commit: creating $branch commit $count"
 
-git commit -m "posion commit $count"
+git commit -m "$branch commit $count"
